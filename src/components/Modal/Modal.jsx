a31a2d7b372318b28e34a039599c6c6ad0c54f75
Dmytro Savych <sava.savych@gmail.com>
2023-07-04 19:08:12 +0300

    add pagination, add button new Car

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { ModalOverlay, ModalContent } from "../Modal/Modal.styled";
+import { ModalOverlay, ModalContent } from "./Modal.styled";
 
-const Modal = ({ onClose, carData, onSave, isDeleteModal, isEditModal }) => {
+const Modal = ({
+  onClose,
+  carData,
+  onSave,
+  isDeleteModal,
+  isEditModal,
+  isAddModal,
+}) => {
   const { handleSubmit, register } = useForm();
 
   const handleFormSubmit = (data) => {
@@ -17,7 +24,23 @@ const Modal = ({ onClose, carData, onSave, isDeleteModal, isEditModal }) => {
     onClose();
   };
 
-  const handleDeleteBtnClik = () => {
+  const handlAddFormSubmit = (data) => {
+    const updatedCar = {
+      ...carData,
+      car: data.company,
+      car_model: data.model,
+      car_model_year: data.year,
+      car_vin: data.vin,
+      car_color: data.color,
+      price: data.price,
+      availability: data.availability,
+    };
+
+    onSave(updatedCar);
+    onClose();
+  };
+
+  const handleDeleteBtnClick = () => {
     onSave();
     onClose();
   };
@@ -25,13 +48,33 @@ const Modal = ({ onClose, carData, onSave, isDeleteModal, isEditModal }) => {
   return (
     <ModalOverlay>
       <ModalContent>
-        <h3>{isEditModal ? "Edit Modal" : "Delete Modal"}</h3>
-        <p>Company: {carData.company}</p>
-        <p>Model: {carData.model}</p>
-        <p>VIN: {carData.vin}</p>
-        <p>Year: {carData.year}</p>
-        {isEditModal && (
-          <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <h3>
+          {isEditModal
+            ? "Edit Modal"
+            : isDeleteModal
+            ? "Delete Modal"
+            : isAddModal
+            ? "Add Modal"
+            : ""}
+        </h3>
+        {isAddModal && (
+          <form onSubmit={handleSubmit(handlAddFormSubmit)}>
+            <label>
+              Company:
+              <input {...register("company")} />
+            </label>
+            <label>
+              Model:
+              <input {...register("model")} />
+            </label>
+            <label>
+              VIN:
+              <input {...register("vin")} />
+            </label>
+            <label>
+              Year:
+              <input {...register("year")} />
+            </label>
             <label>
               Color:
               <input {...register("color")} />
@@ -47,8 +90,37 @@ const Modal = ({ onClose, carData, onSave, isDeleteModal, isEditModal }) => {
             <button type="submit">Save</button>
           </form>
         )}
-        {isDeleteModal && <button onClick={handleDeleteBtnClik}>Delete</button>}
-        <button onClick={onClose}>Close</button>
+        {!isAddModal && (
+          <>
+            <p>Company: {carData.company}</p>
+            <p>Model: {carData.model}</p>
+            <p>VIN: {carData.vin}</p>
+            <p>Year: {carData.year}</p>
+            {isEditModal && (
+              <form onSubmit={handleSubmit(handleFormSubmit)}>
+                <label>
+                  Color:
+                  <input {...register("color")} />
+                </label>
+                <label>
+                  Price:
+                  <input {...register("price")} />
+                </label>
+                <label>
+                  Availability:
+                  <input {...register("availability")} />
+                </label>
+                <button type="submit">Save</button>
+              </form>
+            )}
+          </>
+        )}
+        {!isAddModal && !isDeleteModal && (
+          <button onClick={onClose}>Close</button>
+        )}
+        {isDeleteModal && (
+          <button onClick={handleDeleteBtnClick}>Delete</button>
+        )}
       </ModalContent>
     </ModalOverlay>
   );
